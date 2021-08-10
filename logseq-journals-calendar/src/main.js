bafcf85fd59b24c10d8502496062066da23dd63d
charlie <xyhp915@qq.com>
2021-08-10 17:23:19 +0800

    update calendar trigger ui

--- logseq-journals-calendar/src/main.js
+++ logseq-journals-calendar/src/main.js
@@ -27,29 +27,32 @@ function main () {
   const key = logseq.baseInfo.id
 
   logseq.provideStyle(`
-    @import url("https://at.alicdn.com/t/font_2409735_r7em724douf.css");
+    @import url("https://at.alicdn.com/t/font_2409735_haugsknp36e.css");
     
     div[data-injected-ui=open-calendar-${key}] {
       display: flex;
       align-items: center;
-      opacity: .55;
       font-weight: 500;
       position: relative;
       top: 0px;
     }
     
-    div[data-injected-ui=open-calendar-${key}]:hover {
-      opacity: .9;
+    div[data-injected-ui=open-calendar-${key}] a {
+      opacity: 1;
+      padding: 6px;
+    }
+    
+    div[data-injected-ui=open-calendar-${key}] iconfont {
+      font-size: 18px;
     }
   `)
 
   // external btns
-  logseq.provideUI({
+  logseq.App.registerUIItem('toolbar', {
     key: 'open-calendar',
-    path: '#search',
     template: `
-      <a data-on-click="openCalendar" style="opacity: .6; display: inline-flex; padding-left: 3px; position: relative; bottom: -3px;">
-        <i class="iconfont icon-Calendaralt2"></i>
+      <a class="button" data-on-click="openCalendar">
+        <i class="iconfont icon-calendar"></i>
       </a>
     `,
   })
