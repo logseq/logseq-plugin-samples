1052bd54f93ce22a331f34c492329c94fde703a9
charlie <xyhp915@qq.com>
2025-12-30 21:28:48 +0800

    feat: add TypeScript configuration and update plugin logic for Reddit data fetching

--- logseq-reddit-hot-news/index.ts
+++ logseq-reddit-hot-news/index.ts
@@ -1,20 +1,28 @@
 import '@logseq/libs'
-import { LSPluginBaseInfo } from '@logseq/libs/dist/libs'
+import { LSPluginBaseInfo } from '@logseq/libs/dist/LSPlugin'
 
 const delay = (t = 100) => new Promise(r => setTimeout(r, t))
 
-async function loadRedditData () {
-  const endpoint = 'https://www.reddit.com/r/logseq/hot.json'
+async function loadRedditData() {
+  // const endpoint = 'https://www.reddit.com/r/logseq/hot.json'
+  const endpoint = './hot.json'
+  const { data: { children } } = await fetch(
+    endpoint,
+    {
+      headers: {
+        'User-Agent': 'logseq-reddit-hot-news-plugin',
+        'Accept': 'application/json'
+      }
+    }
+  ).then(res => res.json())
 
-  const { data: { children } } = await fetch(endpoint).then(res => res.json())
   const ret = children || []
 
   return ret.map(({ data }, i) => {
-    const { title, selftext, url, ups, downs, num_comments } = data
-
-    return `${i}. [${title}](${url}) [:small.opacity-50 "🔥 ${ups} 💬 ${num_comments}"]
-collapsed:: true    
-> ${selftext}`
+    const { title, selftext, url, ups, num_comments } = data
+    return `
+    ${i}. [${title}](${url}) [:small.opacity-50 "🔥 ${ups} 💬 ${num_comments}"]
+    **${selftext}**`
   })
 }
 
@@ -22,28 +30,20 @@ collapsed:: true
  * main entry
  * @param baseInfo
  */
-function main (baseInfo: LSPluginBaseInfo) {
+function main(baseInfo: LSPluginBaseInfo) {
   let loading = false
 
   logseq.provideModel({
-    async loadReddits () {
-
-      const info = await logseq.App.getUserConfigs()
+    async loadReddits() {
       if (loading) return
 
       const pageName = 'reddit-logseq-hots-news'
       const blockTitle = (new Date()).toLocaleString()
 
-      logseq.App.pushState('page', { name: pageName })
-
-      await delay(300)
-
+      await logseq.Editor.createPage(pageName, {}, { redirect: true })
+      await delay(500) // wait for page redirect and focus first block
       loading = true
-
       try {
-        const currentPage = await logseq.Editor.getCurrentPage()
-        if (currentPage?.originalName !== pageName) throw new Error('page error')
-
         const pageBlocksTree = await logseq.Editor.getCurrentPageBlocksTree()
         let targetBlock = pageBlocksTree[0]!
 
@@ -59,7 +59,7 @@ function main (baseInfo: LSPluginBaseInfo) {
 
         await logseq.Editor.updateBlock(targetBlock.uuid, `## 🔖 r/logseq - ${blockTitle}`)
       } catch (e) {
-        logseq.App.showMsg(e.toString(), 'warning')
+        await logseq.UI.showMsg(e.toString(), 'warning')
         console.error(e)
       } finally {
         loading = false
